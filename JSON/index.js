0a88d482202bbce78cfa7455627b34ab0636caf6
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-20 12:31:57 +0530

    json

--- JSON/index.js
+++ JSON/index.js
@@ -2,13 +2,23 @@ async function fet_data() {
     let res=await fetch("http://localhost:3000/students")
     let data =await res.json()
     let final_data=data.map((t)=>`
-    <h1>${t.id}</h1>
-    <h1>${t.name}</h1>
-    <h1>${t.age}</h1>
-    <h1>${t.address}</h1>
+    <tr>
+    <td> ${t.id}</td>
+    <td>${t.name}</td>
+    <td>${t.age}</td>
+    <td>${t.address}</td>
+    <td><buttun onclick="mydelete('${t.id}')">Delete</button></td>
+    </tr>
+    
     `).join("")
     document.querySelector('#showdata').innerHTML=final_data
     
 }
 fet_data()
+function mydelete(id){
+    fetch(`http://localhost:3000/students/${id}`,{
+    method:'DELETE'
+})
+.then(re=>alert("deleted successfully..!!!"))
+}
 // map--> it is used for fetch data
